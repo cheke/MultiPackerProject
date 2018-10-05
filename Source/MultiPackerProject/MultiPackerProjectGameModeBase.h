f9e26f9f4b4ff9c14ea52c0bb4067842e524ef34
cheke <turbocheke@gmail.com>
2018-10-05 21:33:22 +0100

    Initial commit

---
+++ Source/MultiPackerProject/MultiPackerProjectGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MultiPackerProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MULTIPACKERPROJECT_API AMultiPackerProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
